5ab0aa3a308c43697fa190992f169afd7383abe5
SteenMaxwell <steen.maxwell@gmail.com>
2024-09-24 22:35:15 +0100

    Adds feature to check letter in random_word for milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,23 @@
+import random
+
+word_list = ['apple', 'banana', 'orange', 'kiwi', 'mango']
+
+print(word_list)
+
+random_word = random.choice(word_list)
+print(random_word)
+
 while True:
-    guess = input('Enter a single letter: ')
+    guess = input('Enter a single letter: ').lower()
     if len(guess) == 1 and guess.isalpha():
         print('Good guess!')
         break
     
     else:
         print('Invalid character. Please enter a single alphabetical character.')
+
+if guess in random_word:
+    print(f'Good guess! {guess} is in the word!')
+
+else:
+    print(f'Sorry, {guess} is not in the word. Try again.')
