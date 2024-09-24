08052b527028de3b7ad9ecfd8c9f1c331f0c336d
SteenMaxwell <steen.maxwell@gmail.com>
2024-09-24 21:47:25 +0100

    adds tasks1-3.py which contains code from tasks 1 to 3 and removes this code from milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,3 @@
-import random
-
-word_list = ['apple', 'banana', 'orange', 'kiwi', 'mango']
-
-print(word_list)
-
-random_word = random.choice(word_list)
-print(random_word)
-
 guess = input('Enter a single letter: ')
 
 if len(guess) == 1 and guess.isalpha():
---
+++ tasks1-3.py
@@ -0,0 +1,8 @@
+import random
+
+word_list = ['apple', 'banana', 'orange', 'kiwi', 'mango']
+
+print(word_list)
+
+random_word = random.choice(word_list)
+print(random_word)
