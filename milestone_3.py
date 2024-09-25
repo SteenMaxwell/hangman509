2fdb10fad3d55ab8eb0c15a91fdee7887c833cc5
SteenMaxwell <steen.maxwell@gmail.com>
2024-09-25 22:22:56 +0100

    Adds two functions to milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -7,16 +7,22 @@ print(word_list)
 random_word = random.choice(word_list)
 print(random_word)
 
-while True:
-    guess = input('Enter a single letter: ').lower()
-    if len(guess) == 1 and guess.isalpha():
-        break
-    
+def check_guess(guess):
+    if guess in random_word:
+        print(f'Good guess! {guess} is in the word!')
+
     else:
-        print('Invalid character. Please enter a single alphabetical character.')
+        print(f'Sorry, {guess} is not in the word. Try again.')
 
-if guess in random_word:
-    print(f'Good guess! {guess} is in the word!')
+def ask_for_input():
+    while True:
+        guess = input('Enter a single letter: ').lower()
+        if len(guess) == 1 and guess.isalpha():
+            break
+    
+        else:
+            print('Invalid character. Please enter a single alphabetical character.')
+    
+    check_guess(guess)
 
-else:
-    print(f'Sorry, {guess} is not in the word. Try again.')
+ask_for_input()
