df8a49cc56c2ffe12a81e339103e0ef1cbb1af1b
SteenMaxwell <steen.maxwell@gmail.com>
2024-09-27 20:26:32 +0100

    Adds milestone_5.py which contains play_game function

---
+++ milestone_5.py
@@ -0,0 +1,116 @@
+import random
+
+class Hangman():
+    '''
+    A play on the classic Hangman game, where the computer chooses a word and the user guesses 
+    a letter to see if it is in this word.
+
+    Methods:
+    -------
+    check_guess(guess)
+        checks if the input guess is in the chosen word.
+    
+    ask_for_input()
+        Asks the user to give a letter.
+    '''
+    
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+        '''
+        Parameters:
+        ----------
+        word_list: list
+            List of words to be used for the game
+        num_lives: int
+            The number of lives the user has
+
+        Attributes:
+        ----------
+        word: str
+            The word to be guessed picked randomly from the word_list
+        word_guessed: list
+            A list of the letters of the word, with '_' for each letter not yet guessed
+        num_letters: int
+            The number of UNIQUE letters in the word that have not been guessed yet
+        list_letters: list
+            A list of the letters that have already been tried
+        '''
+
+    def check_guess(self, guess):
+        '''
+        This function checks if the input guess is in the chosen word.
+        
+        If it is, replaces '_' in word_guessed with the chosen letter.
+        If it is not, user loses a life by -1.
+
+        parameter:
+        ---------
+        guess: str
+            The users input to be checked
+        '''
+    
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good Guess! \'{guess}\' is in the word!')
+            for index, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[index] = guess
+
+            self.num_letters -= 1
+        
+        else:
+            self.num_lives -= 1
+            print(f'Sorry \'{guess}\' is not in the word.')
+            print(f'You have {self.num_lives} lives left!')
+        
+        print(self.word_guessed, '\n')
+        #print('\n')
+
+
+    def ask_for_input(self):
+        '''
+        This function asks the user to give a letter and checks whether the input is valid 
+        and if letter has already been tried.
+        If it passes both checks, the check_guess method is called.
+        '''
+        
+        while True:
+            guess = input('Enter a single letter: ')
+            if len(guess) != 1 or not guess.isalpha():
+                print('Invalid letter. Please enter a single alhpabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+def play_game(word_list):
+    '''
+    This function is used to start the game
+    
+    parameters:
+    ----------
+    word_list: list
+        List of words used for the game
+    '''
+
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    while True:
+        if game.num_lives == 0:
+            print('You lost!')
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print(f'Congratulations, You won! The word was: \'{game.word}\'!')
+            break
+
+word_list = ['apple', 'banana', 'orange', 'kiwi', 'mango']
+play_game(word_list)
